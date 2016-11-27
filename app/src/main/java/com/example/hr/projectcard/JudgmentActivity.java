a604ccfc8b7ecbd00320bb25e111ee01f1c37514
HimanshRusia <himanshrusia93@gmail.com>
2016-11-27 15:37:10 +0530

    CHanges activity

---
+++ app/src/main/java/com/example/hr/projectcard/JudgmentActivity.java
@@ -0,0 +1,13 @@
+package com.example.hr.projectcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JudgmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_judgment);
+    }
+}
