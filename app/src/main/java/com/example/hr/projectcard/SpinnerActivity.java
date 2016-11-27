6b8fe05cfc5055127862254c2b0e652d2b209603
HimanshRusia <himanshrusia93@gmail.com>
2016-11-27 15:39:05 +0530

    Chnages

--- app/src/main/java/com/example/hr/projectcard/SpinnerActivity.java
+++ app/src/main/java/com/example/hr/projectcard/SpinnerActivity.java
@@ -8,7 +8,7 @@ import android.widget.Toast;
 
 public class SpinnerActivity extends GameModeActivity {
     public Spinner spinner1;
-
+    String spin = "";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,16 +18,16 @@ public class SpinnerActivity extends GameModeActivity {
     }
 
     public void addListenerOnSpinnerItemSelection() {
-        final String[] spin = new String[1];
+
         spinner1 = (Spinner) findViewById(R.id.Modes);
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
                         // On selecting a spinner item
-                        spin[0] = adapterView.getItemAtPosition(pos).toString();
+                        spin = adapterView.getItemAtPosition(pos).toString();
                         // Showing selected spinner item
                         Toast.makeText(getApplicationContext(),
-                                "Selected Country : " + spin[0], Toast.LENGTH_SHORT).show();
+                                "Selected Country : " + spin, Toast.LENGTH_SHORT).show();
                     }
                     @Override
                     public void onNothingSelected(AdapterView<?> arg0) {
